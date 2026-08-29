333dcf240ba80c572b38fe86a128f8eb662208ab
Sofia Lopez <sofilopper1338@gmail.com>
2026-08-29 18:10:30 -0300

    Add files via upload

---
+++ js/map-data.js
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------
+// Datos del mini-mapa de Argentina (esquemático, no catastral)
+// Proyección simplificada, calculada a partir de coordenadas
+// aproximadas de cada provincia. Sirve para señalar ubicación,
+// no para uso cartográfico preciso.
+// ---------------------------------------------------------------
+
+const MAP_VIEWBOX = "0 0 380 700";
+
+const MAP_OUTLINE = "M 156.0,40.7 L 198.1,38.9 L 334.1,106.7 L 317.9,140.7 L 285.5,193.5 L 275.8,236.9 L 272.6,274.6 L 295.2,312.3 L 287.1,340.6 L 209.4,359.4 L 167.3,397.1 L 133.3,472.6 L 141.4,529.1 L 102.6,572.5 L 97.7,597.0 L 107.4,615.9 L 112.3,657.4 L 84.8,642.3 L 60.5,604.6 L 42.7,557.4 L 54.0,500.9 L 60.5,438.6 L 60.5,378.3 L 76.7,319.8 L 84.8,276.5 L 88.0,218.0 L 112.3,161.4 L 109.0,99.2 L 128.5,57.7 L 156.0,40.7 Z";
+
+const MAP_COORDS = {"Ciudad Autónoma de Buenos Aires": [272.9, 276.6], "Buenos Aires": [246.7, 321.7], "Catamarca": [153.1, 160.9], "Chaco": [262.0, 141.6], "Chubut": [109.0, 453.7], "Córdoba": [178.7, 227.4], "Corrientes": [265.6, 142.0], "Entre Ríos": [238.1, 222.3], "Formosa": [276.3, 117.7], "Jujuy": [160.9, 69.0], "La Pampa": [157.6, 331.1], "La Rioja": [135.8, 178.6], "Mendoza": [107.4, 265.1], "Misiones": [330.9, 133.1], "Neuquén": [84.8, 351.9], "Río Negro": [141.4, 387.7], "Salta": [165.7, 91.7], "San Juan": [102.6, 204.8], "San Luis": [144.7, 255.7], "Santa Cruz": [84.8, 538.6], "Santa Fe": [232.1, 212.3], "Santiago del Estero": [190.0, 148.2], "Tierra del Fuego, Antártida e Islas del Atlántico Sur": [117.1, 651.7], "Tucumán": [159.2, 131.3]};
